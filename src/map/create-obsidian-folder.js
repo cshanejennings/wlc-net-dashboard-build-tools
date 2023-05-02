285de28e86d460f9ce46e6bcfca985cbc39b67aa
Shane Jennings <cshanejennings@gmail.com>
2023-05-02 16:32:01 -0400

    combined final method into create-obsidian-folder

--- src/map/create-obsidian-folder.js
+++ src/map/create-obsidian-folder.js
@@ -66,4 +66,28 @@ async function crawl_directory(directory) {
   return result;
 }
 
-crawl_directory(path.join(src_path));
+const map_folder = path.join(__dirname, '../../../map');
+const sanitize_content_line = (item) => {
+    item = item.replace("@", "");
+    return (item.indexOf('#') === 0) ? `${item}\n` : `[[${item}]]\n`
+}
+
+const write_data = (data) => {
+    // Create folders and files according to the keys in the data object
+    for (const key in data) {
+        const folder_path = path.join(map_folder, path.dirname(key));
+        fs.mkdirSync(folder_path, { recursive: true });
+
+        const raw_file_path = path.join(map_folder, `${key}.md`);
+        const parts = raw_file_path.split('\\');
+        const file_name = parts.pop()
+            .replace(/\./g, '_')
+            .replace(/_md$/, ".md");
+        const file_path = [...parts, file_name].join('\\');
+        const contents = data[key].map(sanitize_content_line).join('');
+        fs.writeFileSync(file_path, contents)
+    }
+}
+
+const file_map = crawl_directory(path.join(src_path));
+write_data(file_map);
