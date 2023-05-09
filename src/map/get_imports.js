09868c44c0c2e1167d7d7ef36e1af360f920b5c9
Shane Jennings <cshanejennings@gmail.com>
2023-05-09 12:24:58 -0400

    moved json outputs and updated dependency counts

--- src/map/get_imports.js
+++ src/map/get_imports.js
@@ -1,70 +1,84 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const root_path = (path.join(__dirname, '../../../client'));
-const src_path = path.join(root_path, 'src');
-const IMPORT_REGEX = /import\s+(?:{([^}]+)}|([^'";\n]+))\s+from\s+['"]([^'"]+)['"]((?![\s\S]*\/\/)[\s\S]*?(?![\s\S]*\/\*))/gm;
+const root_path = path.join(__dirname, "../../../client");
+const src_path = path.join(root_path, "src");
+const IMPORT_REGEX =
+  /import\s+(?:{([^}]+)}|([^'";\n]+))\s+from\s+['"]([^'"]+)['"]((?![\s\S]*\/\/)[\s\S]*?(?![\s\S]*\/\*))/gm;
 const TARGET_FILE_EXTENSION = /\.(jsx|js|tsx|ts)$/;
 
 const remove_comments = (str) => {
-    return str.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '$1');
-}
+  return str.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, "$1");
+};
 
-const clean_imports = (imports) => imports.split(/,\s*/)
-  .map(s => s.replace(/\{|\}|^type/g, '').trim())
-  .filter(s => (
-    s.length > 0
-    && !s.match(/^\/\/ /)
-));
+const clean_imports = (imports) =>
+  imports
+    .split(/,\s*/)
+    .map((s) => s.replace(/\{|\}|^type/g, "").trim())
+    .filter((s) => s.length > 0 && !s.match(/^\/\/ /));
 
 const get_dependencies = (manual = []) => {
-    const json = JSON.parse(fs.readFileSync(path.join(root_path, 'package.json')));
-    const dependencies = json.dependencies || {};
-    const dev_dependencies = json.devDependencies || {};
-    return [...manual, ...Object.keys(dependencies), ...Object.keys(dev_dependencies)]
-}
+  const json = JSON.parse(
+    fs.readFileSync(path.join(root_path, "package.json"))
+  );
+  const dependencies = json.dependencies || {};
+  const dev_dependencies = json.devDependencies || {};
+  return [
+    ...manual,
+    ...Object.keys(dependencies),
+    ...Object.keys(dev_dependencies),
+  ];
+};
 
-const get_imports = (raw_import_urls) => Array.from(raw_import_urls,
-    ([, namedImports, defaultImport, fromUrl]) => {
+const get_imports = (raw_import_urls) =>
+  Array.from(raw_import_urls, ([, namedImports, defaultImport, fromUrl]) => {
     const imports = clean_imports(namedImports || defaultImport);
     const from = fromUrl.replace(TARGET_FILE_EXTENSION, "");
     return { imports, from };
-});
-const material_ui_core_imports = {};
-const material_ui_lab_imports = {};
-const material_ui_icon_imports = {};
+  });
 
 const add_imported_count = (counter, import_name) => {
-  if (!import_name) { return; }
+  if (!import_name) {
+    return;
+  }
   const count = counter[import_name] ?? 0;
   counter[import_name] = count + 1;
-}
+};
 
-const add_imported = (from, imports, counter) => {
-  if (imports.length === 1 && from.trim().length) {
-    const import_name = from.split('/').pop();
-    add_imported_count(counter, import_name);
+const dependency_counts = {};
+const add_dependency_count = (from, imports, dependency) => {
+  if (!dependency) {
+    return;
   }
-  else if (imports.length) { imports.map(import_name => add_imported_count(counter, import_name))}
-}
+  dependency_counts[dependency] = dependency_counts[dependency] ?? {};
+  if (imports.length === 1 && from.trim().length) {
+    const import_name = from.split("/").pop();
+    add_imported_count(dependency_counts[dependency], import_name);
+  } else if (imports.length) {
+    imports.map((import_name) =>
+      add_imported_count(dependency_counts[dependency], import_name)
+    );
+  }
+};
 
 const dependency_keys = get_dependencies(["prop-types"]);
 const get_src_from_import = ({ from, imports }) => {
-    const dependency = dependency_keys.find((key) => from.startsWith(key));
-    const src = dependency ? `#${dependency}` : from.replace(/\./g, "_");
-    if (from.startsWith('@material-ui/core')) { add_imported(from.replace('@material-ui/core', ''), imports, material_ui_core_imports); }
-    if (from.startsWith('@material-ui/lab')) { add_imported(from.replace('@material-ui/lab', ''), imports, material_ui_lab_imports); }
-    if (from.startsWith('@material-ui/icons')) { add_imported(from.replace('@material-ui/icons', ''), imports, material_ui_icon_imports); }
-    return src;
-    // return dependency ? `#${dependency}` : from.replace(/\./g, "_");
+  const dependency = dependency_keys.find((key) => from.startsWith(key));
+  const src = dependency ? `#${dependency}` : from.replace(/\./g, "_");
+  if (dependency) {
+    add_dependency_count(from, imports, dependency);
+  }
+  return src;
 };
 
 const get_asset_path = (file_path) => {
-    return file_path
-        .replace(/\\/g, '/')
-        .replace(/.*client\//, '')
-        .split('.').slice(0, -1).join('_');
-}
+  return file_path
+    .replace(/\\/g, "/")
+    .replace(/.*client\//, "")
+    .split(".")
+    .slice(0, -1)
+    .join("_");
+};
 
 async function crawl_directory(directory) {
   const get_filepath = (file_name) => path.join(directory, file_name);
@@ -77,32 +91,33 @@ async function crawl_directory(directory) {
       Object.assign(result, subdirResult);
     } else if (file.isFile() && TARGET_FILE_EXTENSION.test(file.name)) {
       const asset_path = get_asset_path(file_path);
-      const file_content = await fs.promises.readFile(file_path, 'utf8');
+      const file_content = await fs.promises.readFile(file_path, "utf8");
       const raw_import = remove_comments(file_content).matchAll(IMPORT_REGEX);
       const imports = get_imports(raw_import);
-      result[asset_path] = [...new Set(imports.flatMap(get_src_from_import).sort())];
+      result[asset_path] = [
+        ...new Set(imports.flatMap(get_src_from_import).sort()),
+      ];
     }
   }
-
-  const json = JSON.stringify(result, null, 2);
-  await fs.promises.writeFile('map.json', json);
   return result;
 }
 
-const crawl_directory = async(path.join(src_path));
+crawl_directory(path.join(src_path)).then((result) => {
+  const show_counts = (name) =>
+    Object.entries(name)
+      .sort((a, b) => (a[0] < b[0] ? -1 : a[0] > b[0] ? 1 : 0))
+      .sort((a, b) => b[1] - a[1])
+      .map(([key, value]) => `${key}: ${value}`);
 
+  const reduce = (obj, name) => {
+    return { ...obj, [name]: show_counts(dependency_counts[name]) };
+  };
+  const pkg_report = Object.keys(dependency_counts).reduce(reduce, {});
 
-// MUI EXPORT SECTION
-const get_count_array = (counter) => Object.entries(counter)
-  .map((import_name) => ({import_name, count: counter[import_name] }))
-  .filter(({import_name}) => (import_name))
-  .sort((a, b) => (b.import_name > a.import_name))
-  // .sort((a, b) => (b.count - a.count))
-  .map(({import_name, count}) => `${import_name}: ${count}`)
-  console.log(material_ui_lab_imports);
-const material_ui_imports = {
-  "@material-ui/core": get_count_array(material_ui_core_imports),
-  "@material-ui/lab": get_count_array(material_ui_lab_imports),
-  "@material-ui/icons": get_count_array(material_ui_icon_imports),
-};
-await fs.promises.writeFile('mui.json', JSON.stringify(material_ui_imports, null, 2));
+  const js_obsidian_filemap_json = JSON.stringify(result, null, 2);
+  fs.promises.writeFile("./json_reports/js_obsidian_filemap.json", js_obsidian_filemap_json);
+
+  const js_package_usage_json = JSON.stringify(pkg_report, null, 2);
+  fs.promises.writeFile("./json_reports/js_package_usage.json", js_package_usage_json);
+
+});
--- src/map/write_imports.js
+++ src/map/write_imports.js
@@ -2,16 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 const map_folder = path.join(__dirname, '../../../map');
-const json_map_path = path.join(__dirname, '../../map.json');
+const js_obsidian_filemap_json_path = path.join(__dirname, '../../json_reports/js_obsidian_filemap.json');
 
 const sanitize_content_line = (item) => {
     item = item.replace("@", "");
     return (item.indexOf('#') === 0) ? `${item}\n` : `[[${item}]]\n`
 }
 
-const write_data = (data) => {
+const write_data = (obsidian_file_map) => {
     // Create folders and files according to the keys in the data object
-    for (const key in data) {
+    for (const key in obsidian_file_map) {
         const folder_path = path.join(map_folder, path.dirname(key));
         fs.mkdirSync(folder_path, { recursive: true });
 
@@ -21,10 +21,10 @@ const write_data = (data) => {
             .replace(/\./g, '_')
             .replace(/_md$/, ".md");
         const file_path = [...parts, file_name].join('\\');
-        const contents = data[key].map(sanitize_content_line).join('');
+        const contents = obsidian_file_map[key].map(sanitize_content_line).join('');
         fs.writeFileSync(file_path, contents)
     }
 }
 
-const data = JSON.parse(fs.readFileSync(json_map_path));
+const data = JSON.parse(fs.readFileSync(js_obsidian_filemap_json_path));
 write_data(data);
