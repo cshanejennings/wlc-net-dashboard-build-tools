5b70f535b714dc495d59c670e1141d43a389c8df
Shane Jennings <cshanejennings@gmail.com>
2023-08-20 02:45:17 -0400

    changed approaches on dto conversion

--- public/js/typescript-to-dto.js
+++ public/js/typescript-to-dto.js
@@ -4,62 +4,81 @@ const DOC = {
   create_dto: document.getElementById("create-dto"),
 };
 
-function extractPropertiesFromTypeScript(tsDeclaration) {
-  const lines = tsDeclaration.split('\n');
-  let className = '';
-  let properties = [];
-  let insideClass = false;
-  let comment = '';
+const REGEX = {
+  comment: /\/\*\*[^*]*\*+(?:[^\/*][^*]*\*+)*\//g,
+  className: /export\s+type\s+([\w\s&]+)\s+=\s*/,
+  property: /^(readonly)?[\s]*([\w]+)[\s]*([\?]*)[\s]*[\:]/,
+  prop_data: /^(readonly)?\s*(\w+)[\s]*([\?]*):[\s]*([\w\s\|\'\"\[\]]+)/,
+};
 
-  lines.forEach((line) => {
-    line = line.trim();
-    if (line.startsWith('/**')) {
-      comment = line;
-    } else if (line.endsWith('*/')) {
-      comment += ' ' + line;
-    } else if (line.startsWith('export type')) {
-      className = line.split(' ')[2];
-      insideClass = true;
-    } else if (insideClass && line.startsWith('}')) {
-      insideClass = false;
-    } else if (insideClass && line.length > 0) {
-      const readonly = line.includes('readonly');
-      const propertyLine = line.split('*/')[1]?.trim() || line;
-      const parts = propertyLine.replace('readonly', '').split(':');
-      if (parts.length < 2) return;
-      const [property, typeInfo] = parts;
-      const nullable = property.includes('?');
-      const propertyName = property.replace('?', '').trim();
-      let phpDocType = typeInfo.split(';')[0].trim().replace('| null', '');
-      let type = typeInfo.split(';')[0].trim().replace('| null', '');
-      // Check if the type contains '|' character, indicating a union of string literals
-      if (type.includes('|')) {
-        type = 'string'; // Treat as string type
-        // Optionally, you can capture the accepted values and store them
-        // const acceptedValues = phpType.split('|').map(value => value.trim().replace(/'/g, '')); 
-      } else {
-        type = type.replace('string', 'string').replace('number', 'int').replace('boolean', 'bool');
-      }
-      type = (type.indexOf('[]') !== -1) ? 'array' : type;
-
-
-      properties.push({
-        comment: comment.replace('/**', ' ').replace('*/', ' ').trim(),
-        property: propertyName,
-        phpDocType,
-        type,
-        nullable,
-        readonly,
-        // acceptedValues, // Optionally include accepted values if needed
-      });
-      comment = ''; // Reset comment
+function get_prop_strings(lines) {
+  const propstrings = [];
+  let prop_lines = [];
+  let section = "prop";
+  const set_prop = () => {
+    propstrings.push(prop_lines.join("\n"));
+    prop_lines = [];
+  };
+  lines.forEach((line, i) => {
+    if (line.startsWith("/**")) {
+      if (prop_lines.length > 0) set_prop();
+      prop_lines.push(line);
+      section = line.endsWith("*/") ? "" : "comment";
+    } else if (section === "comment") {
+      prop_lines.push("  " + line);
+      section = line.endsWith("*/") ? "" : "comment";
+    } else if (section === "comment") {
+      prop_lines.push("  " + line);
+    } else if (section === "prop") {
+      if (line.match(REGEX.property)) set_prop();
+      prop_lines.push(line);
+    } else {
+      prop_lines.push(line);
+      section = "prop";
     }
   });
-
-  return { className, properties };
+  set_prop();
+  return propstrings;
 }
 
-
+function get_props(str) {
+  const com = str.match(REGEX.comment)?.map((c) => c.trim());
+  let className = str.match(REGEX.className)[1].trim();
+  const lines = str
+    .slice(str.indexOf(className))
+    .split("{")[1]
+    .split("}")[0]
+    .split("\n")
+    .map((l) => l.trim())
+    .filter((l) => l.length > 0);
+  const prop_strings = get_prop_strings(lines);
+  const properties = prop_strings.map((str) => {
+    const comment = str
+      .match(REGEX.comment)
+      ?.map((c) => c.trim())
+      .pop();
+    str = str.replace(comment, "").trim().replace(/\n/g, "");
+    const [params, inline_comment] = str.split("//").map((s) => s.trim());
+    const [, readonly, property, nullable, raw_type] = params.match(
+      REGEX.prop_data
+    );
+    const phpDocType = raw_type
+      .replace("string", "string")
+      .replace("number", "int")
+      .replace("boolean", "bool");
+    const type = phpDocType.indexOf("[]") !== -1 ? "array" : (phpDocType.indexOf("|") !== -1) ? 'string' : phpDocType;
+    return {
+      comment: comment.replace("/**", "").trim(),
+      property,
+      phpDocType,
+      type,
+      nullable,
+      readonly,
+      inline_comment,
+    };
+  });
+  return { className, properties };
+}
 
 function createPHPClassTemplate({ className, properties }) {
   let class_props = [];
@@ -67,24 +86,28 @@ function createPHPClassTemplate({ className, properties }) {
   properties.forEach((prop) => {
     const { property, type, phpDocType, nullable, readonly, comment } = prop;
     if (comment) {
-      class_props.push(`  /** @var ${phpDocType} ${comment} */`);
+      class_props.push(`  /** @var ${phpDocType} ${comment}`);
     }
     if (readonly) {
       class_props.push(`  /** @readonly */`);
     }
-    class_props.push(`  public ${nullable ? '?' : ''}${type} $${property};`);
+    class_props.push(`  public ${nullable ? "?" : ""}${type} $${property};`);
     class_props.push(``);
-    
+
     // Check if the type is an array of a specific class
     const arrayMatch = phpDocType.match(/(\w+)\[\]/);
 
     if (arrayMatch) {
       const arrayType = arrayMatch[1];
-      class_constructor.push(`    $this->set_typed_array("${property}", ${arrayType}::class);`);
-    } else if (type === 'int' || type === 'string' || type === 'bool') {
+      class_constructor.push(
+        `    $this->set_typed_array("${property}", ${arrayType}::class);`
+      );
+    } else if (type === "int" || type === "string" || type === "bool") {
       class_constructor.push(`    $this->set_prop("${property}");`);
     } else {
-      class_constructor.push(`    $this->set_typed_prop("${property}", ${type}::class);`);
+      class_constructor.push(
+        `    $this->set_typed_prop("${property}", ${type}::class);`
+      );
     }
   });
   return [
@@ -99,19 +122,16 @@ function createPHPClassTemplate({ className, properties }) {
     `    parent::__construct($data);`,
     ...class_constructor,
     `  }`,
-    '}'
-  ].join('\n');
+    "}",
+  ].join("\n");
 }
 
 function transformTypeScriptToPHP(tsDeclaration) {
-  const { className, properties } = extractPropertiesFromTypeScript(tsDeclaration);
+  const { className, properties } = get_props(tsDeclaration);
   const definition = createPHPClassTemplate({ className, properties });
   DOC.php_dto.textContent = definition;
-  DOC.php_dto.style.display = 'block';
+  DOC.php_dto.style.display = "block";
 }
-
-
-
 
 function createDTOClass(el, outputToggle) {
   const tsDeclaration = el.input.value;
@@ -127,7 +147,7 @@ DOC.create_dto.addEventListener("click", () => {
     toggleOutputs
   );
 });
-DOC.php_dto.addEventListener('click', function() {
+DOC.php_dto.addEventListener("click", function () {
   const selection = window.getSelection();
   const range = document.createRange();
   range.selectNodeContents(this);
